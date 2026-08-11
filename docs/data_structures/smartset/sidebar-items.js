83f49f33b344fd571b38f021e6d9f5f1acc9aa6d
Kyle Simpson <Kyle_Simpson5624@yahoo.com>
2026-08-11 02:47:31 -0500

    Update docs for SmartSet

---
+++ docs/data_structures/smartset/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["SmartSetIntoIter","SmartSetIter"],"struct":["SmartSet"]};
---
+++ docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["data_structures",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"enum\" href=\"data_structures/smartset/enum.SmartSetIter.html\" title=\"enum data_structures::smartset::SmartSetIter\">SmartSetIter</a>&lt;'a, T&gt;",0],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"enum\" href=\"data_structures/smartset/enum.SmartSetIntoIter.html\" title=\"enum data_structures::smartset::SmartSetIntoIter\">SmartSetIntoIter</a>&lt;T&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[751]}
