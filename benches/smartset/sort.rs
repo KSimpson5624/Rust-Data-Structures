fba6b3cacbb827c0c0fc7595d121113bda17e490
Kyle Simpson <Kyle_Simpson5624@yahoo.com>
2026-09-10 01:35:52 -0500

    Add new benchmark for removal

--- benches/smartset/sort.rs
+++ benches/smartset/sort.rs
@@ -77,5 +77,45 @@ fn bench_sort_before_and_after_insert(c: &mut Criterion) {
 
     group.finish();
 }
+fn bench_sort_before_and_after_remove(c: &mut Criterion) {
+    let mut group = c.benchmark_group("sort_twice");
 
-criterion_group!(benches, bench_sort, bench_sort_before_and_after_insert);
+    for size in [1, 100, 1_000, 10_000, 100_000] {
+        let data: Vec<i32> = (0..size).collect();
+
+        // This will take half the vector but round down. Therefore, it is safe for vectors of odd values and of size 1.
+        let item_removed: i32 = (data.len() / 2) as i32;
+
+        group.bench_with_input(BenchmarkId::new("smartset", size), &size, |b, _| {
+            b.iter_batched(||
+                               data.iter().copied().collect::<SmartSet<i32>>(),
+                           |mut set| {
+                               black_box(set.sort());
+                               set.remove(&item_removed);
+                               black_box(set.sort());
+                           },
+                           BatchSize::SmallInput
+            );
+        });
+
+        group.bench_with_input(BenchmarkId::new("hashset", size), &size, |b, _| {
+            b.iter_batched(||
+                               data.iter().copied().collect::<HashSet<i32>>(),
+                           |mut set| {
+                               // To sort a HashSet after insertion, it must be copied a second time to a vector.
+                               let mut vec: Vec<i32> = set.iter().copied().collect();
+                               vec.sort();
+                               set.remove(&item_removed);
+                               vec = set.iter().copied().collect();
+                               vec.sort();
+                               black_box(vec);
+                           },
+                           BatchSize::SmallInput,
+            );
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_sort, bench_sort_before_and_after_insert, bench_sort_before_and_after_remove);
